1b5ea85a591ea748ae86445137d36f49a7e1a36c
chandruk <chandruk@localhost>
2025-10-26 21:32:47 +0530

    commit

---
+++ c2tcrem/src/com/tnsif/day12/InvalidMarksException.java
@@ -0,0 +1,19 @@
+package com.tnsif.day12;
+
+
+
+public class InvalidMarksException extends Exception {
+
+		public InvalidMarksException() {
+			super();//
+		}
+
+		public InvalidMarksException(String message) {
+			super(message);
+		}
+
+		
+}
+	
+	
+
